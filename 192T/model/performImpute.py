a45538ad75f069cd0e898f8bf3222b3dc6b529c4
lightnesscaster <dfj2106@columbia.edu>
2019-07-17 16:33:40 -0400

    Add 192T

---
+++ 192T/model/performImpute.py
@@ -0,0 +1,21 @@
+from data.Final_Combination import tensorCombination
+from dataCleaner import get_sizes
+from data.dataLoader import dataLoader
+
+def main():
+
+    print("Hello there")
+
+    dataObj = dataLoader("time_series.pt", "labels.pt", "masks.pt", "diffs.pt", 32) # Load in the data
+
+    performImpute(dataObj)
+
+    print("Impute completed!")
+
+
+def performImpute(dataObj):
+
+    numPatients, numTimeSteps, numVars = get_sizes(dataObj.data)
+    tensorCombination(dataObj.data, dataObj.masks, numPatients, numTimeSteps, numVars)  
+
+main()
