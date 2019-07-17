370aadb74c2a744ac8da28a2b41a7cafe76212b9
Lolita Nazarov <ln3141592653@gmail.com>
2019-07-17 15:50:51 -0400

    Calculating loss now

--- 192T/model/trainRun.py
+++ 192T/model/trainRun.py
@@ -5,52 +5,68 @@ import sklearn
 from sklearn import metrics
 import numpy as np
 
-def trainRun(model, train_loader, validation_loader, scheduler, optimizer, N_EPOCHS, criterion):
+def trainRun(model, train_loader, validation_loader, scheduler, optimizer, BATCH_SIZE, N_EPOCHS, criterion):
+     
+     loss_vector_training = []
 
-    for epoch in range(N_EPOCHS):
+     for epoch in range(N_EPOCHS):
+            
+            loss_sum = 0
 
-        scheduler.step()
+            scheduler.step()
+    
+            for i, (time_series, labels) in enumerate(train_loader):
 
-        for i, (time_series, labels) in enumerate(train_loader):
+                model.zero_grad()
 
-            model.zero_grad()
+                predictions = model(time_series)
 
-            predictions = model(time_series)
+                loss = criterion(predictions, labels)
 
-            loss = criterion(predictions, labels)
+                loss.backward()
 
-            loss.backward()
-
-            optimizer.step()
-
-        count = 0
-        aucTotal = 0
+                optimizer.step()
+               
+                loss_sum += loss.item()
         
-        for i, (time_series, labels) in enumerate(validation_loader):
+            # divide loss_sum by the number of objects in a batch (total number of objects in training set / N_BATCHES)
 
-            model.zero_grad()
+            loss_total = loss_sum / BATCH_SIZE
+            loss_vector_training.append(loss_total)
 
-            predictions = model(time_series)
+            print('Loss for each Epoch: ', 'Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, N_EPOCHS, loss_total))
 
-            count += 1
+            count = 0
+            aucTotal = 0
+            AUC_vector = []
+ 
+            for i, (time_series, labels) in enumerate(validation_loader):
+
+                model.zero_grad()
+
+                predictions = model(time_series)
+
+                count += 1
             
-            arr = np.sum(labels.data.numpy(),axis=0)
+                arr = np.sum(labels.data.numpy(),axis=0)
             
-            for i in range(len(arr)):
-                if(arr[i]==0):
-                    arr[i]=1
-                    labelsT = torch.transpose(labels, 0, 1)
-                    labelsT[i][0] = 1
-                    labels = torch.transpose(labelsT, 0, 1)
-                elif(arr[i]==model.BATCH_SIZE):
-                    arr[i]=9
-                    labelsT = torch.transpose(labels, 0, 1)
-                    labelsT[i][0]=0
-                    labels = torch.transpose(labelsT, 0, 1)
-            arr = np.sum(labels.data.numpy(), axis = 0)
-            aucTotal += metrics.roc_auc_score(labels, predictions.detach(), average="micro")
+                for i in range(len(arr)):
+                    if(arr[i]==0):
+                        arr[i]=1
+                        labelsT = torch.transpose(labels, 0, 1)
+                        labelsT[i][0] = 1
+                        labels = torch.transpose(labelsT, 0, 1)
+                    elif(arr[i]==model.BATCH_SIZE):
+                        arr[i]=9
+                        labelsT = torch.transpose(labels, 0, 1)
+                        labelsT[i][0]=0
+                        labels = torch.transpose(labelsT, 0, 1)
+                arr = np.sum(labels.data.numpy(), axis = 0) 
+                aucTotal += metrics.roc_auc_score(labels, predictions.detach(), average="micro")
 
-        auc = aucTotal/count
-        print('Epoch [{}/{}], Validation AUC: {}'.format(epoch+1, N_EPOCHS, auc))       
+            auc = aucTotal/count
+            AUC_vector.append(auc)
 
-    return model, optimizer 
+            print('Epoch [{}/{}], Validation AUC: {}'.format(epoch+1, N_EPOCHS, auc))       
+
+     return model, optimizer 
