7bb38169bd0b84ec06c2e71740f345ea94607544
lightnesscaster <dfj2106@columbia.edu>
2019-07-29 14:47:19 -0400

    Add paramsMed

---
+++ 192T/model/paramsMed.py
@@ -0,0 +1,32 @@
+'''
+This class assigns the parameter of the model. This code is what to edit to change these parameters
+'''
+
+import torch
+import torch.nn as nn
+
+class params():
+    
+    def __init__(self, LAMBDA):
+        super(params, self).__init__()
+        
+        self.HIDDEN_DIMENSION = 128
+        self.N_LAYERS = 2
+        self.BATCH_SIZE = 64
+        self.DROPOUT = 0.5
+        self.N_EPOCHS = 30
+        self.LEARNING_RATE = 0.005
+        self.N_FEATURES = 116
+        self.N_CLASSES = 20
+
+        self.criterion = nn.functional.binary_cross_entropy
+       
+        self.optimizer = 0
+        self.scheduler = 0
+
+        self.LAMBDA = LAMBDA
+
+    def setOptimizer(self, model):
+
+        self.optimizer = torch.optim.Adam(model.parameters(), lr=self.LEARNING_RATE)
+        self.scheduler = torch.optim.lr_scheduler.ExponentialLR(self.optimizer, .99)
